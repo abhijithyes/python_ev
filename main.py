4ed1e692e58af5e10757bc1082ea80b26da0301f
abhijith <abhijith@neoito.com>
2023-11-06 02:38:40 +0530

    feat:add jwt token

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from flask import Flask
-from extenssions import db
+from extenssions import db,jwt
+from auth import auth_bp
 
 
 def create_app():
     app = Flask(__name__)
+
+
     app.config.from_prefixed_env()
 
+    
     db.init_app(app)
+    jwt.init_app(app)
+    
+    app.register_blueprint(auth_bp, url_prefix="/auth")
+
+
     return app
--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 from extenssions import db
 from uuid import uuid4
+from werkzeug.security import generate_password_hash,check_password_hash
+
 
 class User(db.Model):
     _tablename_ = 'User'
@@ -10,3 +12,23 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User {self.id} {self.username} "
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+    
+    @classmethod
+    def get_user_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    
